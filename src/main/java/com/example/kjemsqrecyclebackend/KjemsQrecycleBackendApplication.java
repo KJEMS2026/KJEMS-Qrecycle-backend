1f506b314c989ec5f359e406a7340a2d8c1568cf
MU-1995 <jynke58@hotmail.com>
2026-05-01 10:32:37 +0200

    first commit

---
+++ src/main/java/com/example/kjemsqrecyclebackend/KjemsQrecycleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kjemsqrecyclebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjemsQrecycleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KjemsQrecycleBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kjemsqrecyclebackend/KjemsQrecycleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kjemsqrecyclebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjemsQrecycleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
