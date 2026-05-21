8270620091a4742d44ea8a0e257a6b27e181e235
404BrainNotFound1000 <emilgurres@gmail.com>
2026-05-21 22:23:16 +0200

    test: added test setup environment for PickupRequestTest.java

---
+++ src/test/java/com/example/kjemsqrecyclebackend/controller/PickuRequestControllerTest.java
@@ -0,0 +1,89 @@
+package com.example.kjemsqrecyclebackend.controller;
+
+import com.example.kjemsqrecyclebackend.DTOMapper.PickupRequestMapper;
+import com.example.kjemsqrecyclebackend.dto.DriverPickupRequestDTO;
+import com.example.kjemsqrecyclebackend.entity.Company;
+import com.example.kjemsqrecyclebackend.entity.PickupRequest;
+import com.example.kjemsqrecyclebackend.entity.User;
+import com.example.kjemsqrecyclebackend.entity.UserRole;
+import com.example.kjemsqrecyclebackend.service.IPickupRequestService;
+import com.example.kjemsqrecyclebackend.service.PickupRequestService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@WebMvcTest
+public class PickuRequestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @MockitoBean
+    private IPickupRequestService iPickupRequestService;
+
+    @MockitoBean
+    private PickupRequestService pickupRequestService;
+
+    @MockitoBean
+    private PickupRequestMapper pickupRequestMapper;
+
+    private DriverPickupRequestDTO testDriverPickupRequestDTO;
+    private PickupRequest testPickupRequest;
+    private Company testCompany;
+    private User testUser;
+    private UserRole testUserRole;
+
+    @BeforeEach
+    void setupUp() {
+        // user er der først, fordi company har brug for User mht. @OneToOne = nullable=false
+        testUserRole = UserRole.DRIVER;
+
+        //user DTO
+        testUser = new User();
+        testUser.setId(UUID.fromString("11111111-1111-1111-1111-111111111111"));
+        testUser.setFirstName("Preben");
+        testUser.setLastName("Daddelballe");
+        testUser.setEmail("Balle.preben@test.com");
+        testUser.setPhonenumber("+4512345678");
+        testUser.setRole(testUserRole);
+
+        //Company instance
+        testCompany = new Company();
+        testCompany.setId(1);
+        testCompany.setCompanyName("Macdonalds Glostrup");
+        testCompany.setAddress("Amager Strandvej 100, 2300 Copenhagen");
+        testCompany.setUser(testUser);
+
+        //Pickuprequst instance
+        testPickupRequest = new PickupRequest();
+        testPickupRequest.setId(100);
+        testPickupRequest.setCompany(testCompany);
+        testPickupRequest.setUser(testUser);
+        testPickupRequest.setDateCreation(LocalDateTime.now().minusDays(2));
+        testPickupRequest.setDateCollected(null);
+        testPickupRequest.setBagsToBeCollected(10);
+        testPickupRequest.setBagsCollected(null);
+
+        //pickupRequstDTO instance
+        testDriverPickupRequestDTO = new DriverPickupRequestDTO();
+        testDriverPickupRequestDTO.setPickupRequestId(100);
+        testDriverPickupRequestDTO.setBagsPickedUp(8);
+        testDriverPickupRequestDTO.setBagsForPickup(10);
+        testDriverPickupRequestDTO.setDifferenceInBags(
+                testDriverPickupRequestDTO.getBagsForPickup() - testDriverPickupRequestDTO.getBagsPickedUp()
+        );
+
+        testDriverPickupRequestDTO.setCreationDate(LocalDateTime.now().minusDays(2));
+        testDriverPickupRequestDTO.setPickupDate(LocalDateTime.now());
+
+        testDriverPickupRequestDTO.setCompany(testCompany);
+        testDriverPickupRequestDTO.setPickedUpBy(testUser);
+
+    }
+}
